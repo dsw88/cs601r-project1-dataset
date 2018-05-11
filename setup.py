b01a8ebbd54c247e99699d3ff2576a63ff414549
David Woodruff <dswoodruff88@gmail.com>
2018-05-11 09:44:38 -0600

    Add images to packge

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='cs601r-birds-butterflies-dataset',
-    version='0.0.1',
+    name='cs601r-project1-dataset',
+    version='0.0.2',
     author='David Woodruff',
     author_email='dswoodruff88@gmail.com',
     packages=['dataset'],
-    url='http://pypi.python.org/pypi/cs601r-birds-butterflies-dataset/',
+    url='http://pypi.python.org/pypi/cs601r-project1-dataset/',
     license='LICENSE.txt',
     description='Dataset used in the CS601R class',
     long_description=open('README.txt').read(),
